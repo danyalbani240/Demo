10d582793136f393f9652295abd206c117b1be44
DanyalBani <mr.d.bani.d@gmail.com>
2026-05-09 07:57:07 +0330

    deleting unnecassary

--- app/plugins/00-auth-init.ts
+++
@@ -1,9 +0,0 @@
-export default defineNuxtPlugin(async () => {
-  const auth = useAuthStore();
-  if (auth.ready || auth.initializing) return;
-  try {
-    await auth.init();
-  } catch (e) {
-    console.log(e);
-  }
-});
--- app/plugins/loadBootstrap.global.ts
+++ app/plugins/loadBootstrap.global.ts
@@ -1,7 +1,10 @@
 export default defineNuxtPlugin(async () => {
   let bootStrapStore = useBootstrapStore();
+  const auth = useAuthStore();
   try {
     await bootStrapStore.fetchBootstrap();
+    if (auth.ready || auth.initializing) return;
+    await auth.init();
   } catch (e) {
     console.log(e);
   }
